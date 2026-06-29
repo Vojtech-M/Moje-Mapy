ce97d03649c99f757828345542f14f77237b0524
Vojtěch Michal <vojtamichal04@gmail.com>
2026-06-29 15:49:41 +0200

    change the name of link

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+              <a href="hhttps://vojtech-m.github.io/" target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"

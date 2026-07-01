4e18a3a00003d8aef3b6e616ca5262d72d34313a
Vojtěch Michal <vojtamichal04@gmail.com>
2026-07-01 18:48:16 +0200

    change link name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/muj-react-projekt/', // <-- TADY MUSÍ BÝT JEN NÁZEV SLOŽKY V LOMÍTKÁCH, NE LINK NA GITHUB CODES!
+  base: '/Moje-Mapy/', // <-- TADY MUSÍ BÝT JEN NÁZEV SLOŽKY V LOMÍTKÁCH, NE LINK NA GITHUB CODES!
 })
